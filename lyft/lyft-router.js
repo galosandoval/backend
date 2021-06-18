b8f5b22b9825796abdf05c9d18fe66ba93eef08f
Galo Sandoval <galosan92@gmail.com>
2021-06-17 23:56:49 -0700

    finished functionality

--- lyft/lyft-router.js
+++ lyft/lyft-router.js
@@ -1,30 +1,32 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
 router.post("/", (req, res) => {
-  const string = req.body.body
+  const string = req.body["string_to_cut"];
+  console.log(string)
 
-  
   const reduceString = (stringToCut) => {
-    let string = stringToCut.trim().replace(/ /g, "")
-    let stringCount = string.length
-    let ansStr = ''
-    let beginIndex = 2
-      
-    while (stringCount > 0) {
-      const endIndex = beginIndex + 1
-      const stringToSlice = string
-      const stringToAdd = stringToSlice.slice(beginIndex, endIndex)
-      ansStr = ansStr + stringToAdd
-      stringCount -= 3
-      beginIndex += 3
-    }
-    return ansStr
-  }
-  const ans = reduceString(string)
-  res.status(201).json({return_string: ans})
+    let trimmedString = stringToCut.trim().replace(/ /g, "");
+    
+    let stringCount = trimmedString.length;
+    let ansStr = "";
+    let beginIndex = 2;
 
-})
+    while (stringCount > 0) {
+      const endIndex = beginIndex + 1;
+      const stringToSlice = trimmedString;
+      const stringToAdd = stringToSlice.slice(beginIndex, endIndex);
+      ansStr = ansStr + stringToAdd;
+      stringCount -= 3;
+      beginIndex += 3;
+    }
+
+    return ansStr;
+  };
+
+  const ans = reduceString(string);
+  res.status(201).json({ return_string: ans });
+});
 
 module.exports = router;
